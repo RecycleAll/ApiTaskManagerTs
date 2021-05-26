438178dacd9f9d607c9f7586b8be28792f502df2
nathanaelto <nathanael.to@free.fr>
2021-05-26 17:06:43 +0200

    Update security.middleware

--- src/middlewares/security.middleware.ts
+++ src/middlewares/security.middleware.ts
@@ -1,5 +1,10 @@
 import express from "express";
 
 export async function securityMiddleware(req: express.Request, res: express.Response, next: express.NextFunction){
-    next()
+    const auth = req.headers["authorization"];
+    if (auth !== undefined){
+
+    }
+
+    next();
 }
